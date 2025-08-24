5f16cf9cdd6ec44406091f78967a8864f5160bd0
Swati <154252045+swatified@users.noreply.github.com>
2025-08-24 21:01:43 +0530

    day1: strings update

--- ConcatnReverse.java
+++ ConcatnReverse.java
@@ -12,3 +12,16 @@ class Solution {
         return r;
     }
 }
+
+
+
+// OPTIMAL ANSWER: 
+
+class Solution {
+    static String conRevstr(String S1, String S2) {
+        // code here
+        StringBuilder rev = new StringBuilder();
+        rev.append(S1+S2);
+        return rev.reverse().toString();
+    }
+}
